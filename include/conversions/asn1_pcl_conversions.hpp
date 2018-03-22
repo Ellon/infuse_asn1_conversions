b0e4ffd6b09097f9b362bdf21bcb09b8da828036
Ellon Paiva Mendes <ellon.mendes@laas.fr>
2018-03-22 22:43:54 +0100

    (split from CDFF) [Conversions] Add fromASN1SCC functions for some pcl types

--- include/conversions/asn1_pcl_conversions.hpp
+++ include/conversions/asn1_pcl_conversions.hpp
@@ -20,10 +20,18 @@
 
 void toASN1SCC(const pcl::uint64_t& pcl_stamp, Time& time);
 
+void fromASN1SCC(const Time& time, pcl::uint64_t& pcl_stamp);
+
 void toASN1SCC(const pcl::PCLHeader& pcl_header, Header& header);
+
+void fromASN1SCC(const Header& header, pcl::PCLHeader& pcl_header);
 
 void toASN1SCC(const pcl::PointXYZ& pcl_pt, Point& pt);
 
+void fromASN1SCC(const Point& pt, pcl::PointXYZ& pcl_pt);
+
 void toASN1SCC(const pcl::PointCloud<pcl::PointXYZ>& pcl_cloud, const PoseStamped& pose_stamped, PointCloudPoseStamped& cloud);
+
+void fromASN1SCC(const PointCloudPoseStamped& cloud, pcl::PointCloud<pcl::PointXYZ>& pcl_cloud, PoseStamped& pose_stamped);
 
 #endif // _ASN1SCC_PCL_CONVERSIONS_H_
