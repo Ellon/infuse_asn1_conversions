d34bfa6f5cdc7497616f8cd6a65226cb5819ec08
Ellon Paiva Mendes <ellon.mendes@laas.fr>
2018-06-08 15:55:23 +0200

    (split from CDFF) [Conversions] Change the convertion to Pose_Infuse to TransformWithCovariance

--- include/conversions/asn1_eigen_conversions.hpp
+++ include/conversions/asn1_eigen_conversions.hpp
@@ -10,7 +10,7 @@
 // ASN1 types 
 // -----------
 #include <Pose.h>
-#include <Pose_InFuse.h>
+#include <TransformWithCovariance.h>
 
 
 void toASN1SCC(const Eigen::Vector3f& eigen_vector, Position& pos);
@@ -28,24 +28,22 @@ void fromASN1SCC(const Orientation& orient, Eigen::Matrix3f& eigen_rot_matrix);
 void toASN1SCC(const Eigen::Affine3f& eigen_transform, Pose& pose);
 
 template<class EIGEN_TRANSFORM>
-void toASN1SCC(const EIGEN_TRANSFORM& eigen_transform, Pose_InFuse& pose)
+void toASN1SCC(const EIGEN_TRANSFORM& eigen_transform, TransformWithCovariance& transform)
 {
-	// NOTE: pose.transform.cov not set in this function!
-
-	pose.msgVersion = pose_InFuse_Version;
+	// NOTE: transform.cov not set in this function!
 
 	typename EIGEN_TRANSFORM::VectorType t(eigen_transform.translation());
-	pose.transform.translation.arr[0] = t[0];
-	pose.transform.translation.arr[1] = t[1];
-	pose.transform.translation.arr[2] = t[2];
-	pose.transform.translation.nCount = 3;
+	transform.translation.arr[0] = t[0];
+	transform.translation.arr[1] = t[1];
+	transform.translation.arr[2] = t[2];
+	transform.translation.nCount = 3;
 
 	Eigen::Quaternion<typename EIGEN_TRANSFORM::Scalar> quat(eigen_transform.rotation());
-	pose.transform.orientation.arr[0] = quat.x();
-	pose.transform.orientation.arr[1] = quat.y();
-	pose.transform.orientation.arr[2] = quat.z();
-	pose.transform.orientation.arr[3] = quat.w();
-	pose.transform.orientation.nCount = 4;
+	transform.orientation.arr[0] = quat.x();
+	transform.orientation.arr[1] = quat.y();
+	transform.orientation.arr[2] = quat.z();
+	transform.orientation.arr[3] = quat.w();
+	transform.orientation.nCount = 4;
 }
 
 
