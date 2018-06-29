d2398e9f1098be501e70607f573fdec418bbcc2f
Pierre Narvor <pnarvor@laas.fr>
2018-06-29 14:14:47 +0200

    (split from CDFF) [Conversions] Fixed string conversion

--- src/asn1_conversions.cpp
+++ src/asn1_conversions.cpp
@@ -8,12 +8,12 @@ void toASN1SCC(const std::string& str, T_String& t_str)
 	// TODO: Consider zero-byte character at the end of string?
 	if (str.size() > maxSize_T_String)
 		throw std::runtime_error("string is longer than maxSize_T_String");
-	memcpy(t_str.arr, str.c_str(), str.size());
-	t_str.nCount = str.size();
+	memcpy(t_str.arr, str.c_str(), str.size() + 1);
+	t_str.nCount = str.size() + 1;
 }
 
 void fromASN1SCC(const T_String& t_str, std::string& str)
 {
-	str = std::string((const char*)t_str.arr, (size_t)t_str.nCount);
+	//str = std::string((const char*)t_str.arr, (size_t)t_str.nCount);
 	str = std::string((const char*)t_str.arr);//, (size_t)t_str.nCount);
 }
