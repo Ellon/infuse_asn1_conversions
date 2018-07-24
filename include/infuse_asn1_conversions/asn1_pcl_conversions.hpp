3b65880c4f5f80c832be0ab04c7583912e1e16de
Ellon Paiva Mendes <ellon.mendes@laas.fr>
2018-07-24 16:33:04 +0200

    Remove deprecated convertion functions (these types do not exist anymore)

--- include/infuse_asn1_conversions/asn1_pcl_conversions.hpp
+++ include/infuse_asn1_conversions/asn1_pcl_conversions.hpp
@@ -6,16 +6,12 @@
 // -----------
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
-#include <pcl/PCLHeader.h>
 
 // -----------
 // ASN1 types 
 // -----------
 #include <infuse_cdff_types/Time.h>
-#include <infuse_cdff_types/Header.h>
 #include <infuse_cdff_types/Point.h>
-#include <infuse_cdff_types/PoseStamped.h>
-#include <infuse_cdff_types/PointCloudPoseStamped.h>
 #include <infuse_cdff_types/PointCloud_InFuse.h>
 
 
@@ -23,17 +19,9 @@ void toASN1SCC(const pcl::uint64_t& pcl_stamp, Time& time);
 
 void fromASN1SCC(const Time& time, pcl::uint64_t& pcl_stamp);
 
-void toASN1SCC(const pcl::PCLHeader& pcl_header, Header& header);
-
-void fromASN1SCC(const Header& header, pcl::PCLHeader& pcl_header);
-
 void toASN1SCC(const pcl::PointXYZ& pcl_pt, Point& pt);
 
 void fromASN1SCC(const Point& pt, pcl::PointXYZ& pcl_pt);
-
-void toASN1SCC(const pcl::PointCloud<pcl::PointXYZ>& pcl_cloud, const PoseStamped& pose_stamped, PointCloudPoseStamped& cloud);
-
-void fromASN1SCC(const PointCloudPoseStamped& cloud, pcl::PointCloud<pcl::PointXYZ>& pcl_cloud, PoseStamped& pose_stamped);
 
 void toASN1SCC(const pcl::PointXYZI& pcl_pt, Point& pt);
 
--- src/asn1_pcl_conversions.cpp
+++ src/asn1_pcl_conversions.cpp
@@ -16,18 +16,6 @@ void fromASN1SCC(const Time& time, pcl::uint64_t& pcl_stamp)
 	pcl_stamp = (pcl::uint64_t)time.microseconds;
 }
 
-void toASN1SCC(const pcl::PCLHeader& pcl_header, Header& header)
-{
-	toASN1SCC(pcl_header.frame_id, header.frameId);
-	toASN1SCC(pcl_header.stamp,    header.timeStamp);
-}
-
-void fromASN1SCC(const Header& header, pcl::PCLHeader& pcl_header)
-{
-	fromASN1SCC(header.frameId,   pcl_header.frame_id);
-	fromASN1SCC(header.timeStamp, pcl_header.stamp);
-}
-
 void toASN1SCC(const pcl::PointXYZ& pcl_pt, Point& pt)
 {
 	pt.arr[0] = pcl_pt.x;
@@ -60,30 +48,6 @@ void fromASN1SCC(const Point& pt, pcl::PointXYZI& pcl_pt)
 	pcl_pt.x = pt.arr[0];
 	pcl_pt.y = pt.arr[1];
 	pcl_pt.z = pt.arr[2];
-}
-
-void toASN1SCC(const pcl::PointCloud<pcl::PointXYZ>& pcl_cloud, const PoseStamped& pose_stamped, PointCloudPoseStamped& cloud)
-{
-	toASN1SCC(pcl_cloud.header, cloud.header);
-	// TODO: Fix this attribution, as ASN1 types are structors with pointers,
-	// and trivial attribution may copy the pointers and lead to bugs
-	cloud.pose = pose_stamped;
-	cloud.pointCloudData.nCount = std::min((T_UInt32)pcl_cloud.points.size(), maxSizePointCloud);
-	for(int i = 0; i < cloud.pointCloudData.nCount; i++)
-		toASN1SCC(pcl_cloud.points[i], cloud.pointCloudData.arr[i]);
-}
-
-void fromASN1SCC(const PointCloudPoseStamped& cloud, pcl::PointCloud<pcl::PointXYZ>& pcl_cloud, PoseStamped& pose_stamped)
-{
-	fromASN1SCC(cloud.header, pcl_cloud.header);
-	// TODO: Fix this attribution, as ASN1 types are structors with pointers,
-	// and trivial attribution may copy the pointers and lead to bugs
-	pose_stamped = cloud.pose;
-	pcl_cloud.points.resize(cloud.pointCloudData.nCount);
-	for(unsigned int i = 0; i < pcl_cloud.points.size(); i++)
-	{
-		fromASN1SCC(cloud.pointCloudData.arr[i], pcl_cloud.points[i]);
-	}
 }
 
 void toASN1SCC(const pcl::PointCloud<pcl::PointXYZI>& pcl_cloud, PointCloud_InFuse& cloud)
