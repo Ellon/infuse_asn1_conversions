43b901571efe2a8942f2b7968ebf172b7c727b2f
Quentin Labourey <quentin.labourey@laas.fr>
2018-08-20 12:37:16 +0200

    Updated pom_conversions to new asn1 types

--- include/infuse_asn1_conversions/asn1_pom_conversions.hpp
+++ include/infuse_asn1_conversions/asn1_pom_conversions.hpp
@@ -1,11 +1,11 @@
 #ifndef DEF_ASN1SCC_POSITIONMANAGER_CONVERSIONS_H
 #define DEF_ASN1SCC_POSITIONMANAGER_CONVERSIONS_H
 
-#include <Pose_InFuse.h>
-#include <PositionManagerBase.hpp>
+#include <infuse_asn1_types/TransformWithCovariance.h>
+#include <infuse_pom_base/PositionManagerBase.hpp>
 
-void toASN1SCC(const PositionManager::Pose& pose, Pose_InFuse& asnPose);
-void fromASN1SCC(const Pose_InFuse& asnPose, PositionManager::Pose& pose);
+void toASN1SCC(const PositionManager::Pose& pose, asn1SccTransformWithCovariance& asnPose);
+void fromASN1SCC(const asn1SccTransformWithCovariance& asnPose, PositionManager::Pose& pose);
 
 #endif
 
--- src/asn1_pom_conversions.cpp
+++ src/asn1_pom_conversions.cpp
@@ -1,62 +1,62 @@
-#include "conversions/asn1_pom_conversions.hpp"
-#include "conversions/asn1_conversions.hpp"
+#include "infuse_asn1_conversions/asn1_pom_conversions.hpp"
+#include "infuse_asn1_conversions/asn1_base_conversions.hpp"
 
 using namespace std;
 
-void toASN1SCC(const PositionManager::Pose& pose, Pose_InFuse& asnPose)
+void toASN1SCC(const PositionManager::Pose& pose, asn1SccTransformWithCovariance& asnPose)
 {
-    asnPose.msgVersion = pose_InFuse_Version;
+    asnPose.metadata.msgVersion = transformWithCovariance_version;
 
-    toASN1SCC(pose._parent, asnPose.parentFrameId);
-    asnPose.parentTime.microseconds = pose._parentTime;
-    asnPose.parentTime.usecPerSec = PositionManager::TimeManager::USEC_PER_SEC;
-    toASN1SCC(pose._child, asnPose.childFrameId);
-    asnPose.childTime.microseconds  = pose._childTime;
-    asnPose.childTime.usecPerSec = PositionManager::TimeManager::USEC_PER_SEC;
+    // Convert metadata 
+    toASN1SCC(pose._parent, asnPose.metadata.parentFrameId);
+    asnPose.metadata.parentTime.microseconds = pose._parentTime;
+    asnPose.metadata.parentTime.usecPerSec = PositionManager::TimeManager::USEC_PER_SEC;
+    toASN1SCC(pose._child, asnPose.metadata.childFrameId);
+    asnPose.metadata.childTime.microseconds  = pose._childTime;
+    asnPose.metadata.childTime.usecPerSec = PositionManager::TimeManager::USEC_PER_SEC;
     
-    asnPose.transform.translation.nCount = 3;
-    asnPose.transform.translation.arr[0] = pose._tr.transform.translation(0);
-    asnPose.transform.translation.arr[1] = pose._tr.transform.translation(1);
-    asnPose.transform.translation.arr[2] = pose._tr.transform.translation(2);
+    // Convert data 
+    asnPose.data.translation.arr[0] = pose._tr.transform.translation(0);
+    asnPose.data.translation.arr[1] = pose._tr.transform.translation(1);
+    asnPose.data.translation.arr[2] = pose._tr.transform.translation(2);
 
-    asnPose.transform.orientation.nCount = 4;
-    asnPose.transform.orientation.arr[0] = pose._tr.transform.orientation.x();
-    asnPose.transform.orientation.arr[1] = pose._tr.transform.orientation.y();
-    asnPose.transform.orientation.arr[2] = pose._tr.transform.orientation.z();
-    asnPose.transform.orientation.arr[3] = pose._tr.transform.orientation.w();
+    asnPose.data.orientation.arr[0] = pose._tr.transform.orientation.x();
+    asnPose.data.orientation.arr[1] = pose._tr.transform.orientation.y();
+    asnPose.data.orientation.arr[2] = pose._tr.transform.orientation.z();
+    asnPose.data.orientation.arr[3] = pose._tr.transform.orientation.w();
 
-    asnPose.transform.cov.nCount = 6;
     for(int i = 0; i < 6; i++)
     {
-        asnPose.transform.cov.arr[i].nCount = 6;
         for(int j = 0; j < 6; j++)
         {
-            asnPose.transform.cov.arr[i].arr[j] = pose._tr.transform.cov(i,j);
+            asnPose.data.cov.arr[i].arr[j] = pose._tr.transform.cov(i,j);
         }
     }
 }
 
-void fromASN1SCC(const Pose_InFuse& asnPose, PositionManager::Pose& pose)
+void fromASN1SCC(const asn1SccTransformWithCovariance& asnPose, PositionManager::Pose& pose)
 {
-    fromASN1SCC(asnPose.parentFrameId, pose._parent);
-    pose._parentTime = asnPose.parentTime.microseconds;
-    fromASN1SCC(asnPose.childFrameId, pose._child);
-    pose._childTime = asnPose.childTime.microseconds;
+    // Convert metadata 
+    fromASN1SCC(asnPose.metadata.parentFrameId, pose._parent);
+    pose._parentTime = asnPose.metadata.parentTime.microseconds;
+    fromASN1SCC(asnPose.metadata.childFrameId, pose._child);
+    pose._childTime = asnPose.metadata.childTime.microseconds;
 
-    pose._tr.transform.translation(0) = asnPose.transform.translation.arr[0];
-    pose._tr.transform.translation(1) = asnPose.transform.translation.arr[1];
-    pose._tr.transform.translation(2) = asnPose.transform.translation.arr[2];
+    // Convert data
+    pose._tr.transform.translation(0) = asnPose.data.translation.arr[0];
+    pose._tr.transform.translation(1) = asnPose.data.translation.arr[1];
+    pose._tr.transform.translation(2) = asnPose.data.translation.arr[2];
     
-    pose._tr.transform.orientation.x() = asnPose.transform.orientation.arr[0];
-    pose._tr.transform.orientation.y() = asnPose.transform.orientation.arr[1];
-    pose._tr.transform.orientation.z() = asnPose.transform.orientation.arr[2];
-    pose._tr.transform.orientation.w() = asnPose.transform.orientation.arr[3];
+    pose._tr.transform.orientation.x() = asnPose.data.orientation.arr[0];
+    pose._tr.transform.orientation.y() = asnPose.data.orientation.arr[1];
+    pose._tr.transform.orientation.z() = asnPose.data.orientation.arr[2];
+    pose._tr.transform.orientation.w() = asnPose.data.orientation.arr[3];
     
     for(int i = 0; i < 6; i++)
     {
         for(int j = 0; j < 6; j++)
         {
-            pose._tr.transform.cov(i,j) = asnPose.transform.cov.arr[i].arr[j];
+            pose._tr.transform.cov(i,j) = asnPose.data.cov.arr[i].arr[j];
         }
     }
 }
